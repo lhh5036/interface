31ffdf7f28a1cd116471bd1c2e79b491d434cf57
majiaqin <majiaqin@techb.top>
2021-09-04 16:52:31 +0800

    新增财务系统涉及公共配置

---
+++ apps/Fmis/fmis_interface_service/fmisSystem_comConfig.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: fmisSystem_comConfig.py
+@time:2021/9/4
+@Author:majiaqin 170479
+@Desc:财务系统涉及公共配置等
+'''
+
+class Fmis_Common_Config:
+    pass
