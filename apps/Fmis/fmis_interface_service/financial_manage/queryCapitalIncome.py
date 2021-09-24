bfb01850616717eaff069cf7b5b89ffe42e5b31a
majiaqin <majiaqin@techb.top>
2021-09-24 17:28:18 +0800

    增加错误异常日志打印

--- apps/Fmis/fmis_interface_service/financial_manage/queryCapitalIncome.py
+++ apps/Fmis/fmis_interface_service/financial_manage/queryCapitalIncome.py
@@ -21,7 +21,7 @@ logger = MyLog("QueryCapitalIncome").getlog()
 
 # 查询资金收入日报表列表数据接口
 class QueryCapitalIncome():
-    def __init__(self, url=InterfaceCommonInfo.common_url,
+    def __init__(self, url=FmisApiUrl.fundReport_query_url,
                  header=Common_TokenHeader().common_header):
         # 初始化url和header
         self.url = url
@@ -41,16 +41,22 @@ class QueryCapitalIncome():
             form03[k] = paramList[i]
             i += 1
         form02 = FmisApiInputParam.capital_income02
-        form02["search"] = json.dumps(form03)
+        form02["search"] = form03
         form01 = FmisApiInputParam.capital_income01
-        form01["args"] = json.dumps(form02)
-        self.form = form01
-
-        resp = requests.post(self.url, headers=self.header, data=self.form)
-        if resp.json()["success"] == True:
-            logger.info("querycapitalincome ---->end!")
-            return "查询资金收入日报表列表数据--接口响应成功"
-        else:
-            logger.error("querycapitalincome ----> response Data is wrong!")
-            return "接口响应失败,失败原因:{0},\n接口地址:{1},\n请求参数:{2}".format(resp.json(),
-                                                                  self.url, self.form)
+        form01["args"] = json.dumps(form02, ensure_ascii=False)
+        self.form = json.dumps(form01, ensure_ascii=False)
+        # self.form = self.form.encode("utf-8").decode("latin1")
+        print(self.form)
+        resp = requests.post(self.url, headers=self.header, data=self.form.encode())
+        print(resp.json())
+        try:
+            if resp.json()["success"] == True:
+                logger.info("querycapitalincome ---->end!")
+                return "查询资金收入日报表列表数据--接口响应成功"
+            else:
+                logger.error("querycapitalincome ----> response Data is wrong!")
+                return "接口响应失败,失败原因:{0},\n接口地址:{1},\n请求参数:{2}".format(resp.json(),
+                                                                      self.url, self.form)
+        except KeyError:
+            logger.error("querycapitalincome ----> {0}".format(resp.json()))
+            return resp.json()
