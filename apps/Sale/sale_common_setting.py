23fcba122af9bc50ce0dad6ae368835882447131
majiaqin <majiaqin@techb.top>
2021-10-06 16:05:31 +0800

    新增订单系统模块

---
+++ apps/Sale/sale_common_setting.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: sale_common_setting
+@time:2021/10/6
+@Author:majiaqin 170479
+@Desc:订单系统全局配置
+'''
+
+from apps.Common_Config.db_config import ReadConfig
+
+class Sale_Common_Setting:
+    # 测试环境MySQL数据库配置
+    sale_mysql = ReadConfig().getDbConfig("test", "erp_oms", "mysql")
+
+    # 生产环境MySQL数据库配置
+    # sale_mysql = ReadConfig().getDbConfig("release", "erp_oms", "mysql")
+
+if __name__ == '__main__':
+    s = Sale_Common_Setting.sale_mysql
+    print(s)
