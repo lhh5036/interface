4ec1ef30d674ec8c161946b2cf709a2c0b66ced4
majiaqin <majiaqin@techb.top>
2021-10-06 14:31:03 +0800

    增加财务系统局域配置项，统一切换

--- apps/Fmis/fmis_common_settting.py
+++ apps/Fmis/fmis_common_settting.py
@@ -1,6 +1,21 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
 '''
 @File: fmis_common_settting
 @time:2021/10/6
 @Author:majiaqin 170479
-@Desc:
+@Desc:财务系统全局配置
 '''
+
+from apps.Common_Config.db_config import ReadConfig
+
+class Fmis_Common_Setting:
+    # 测试环境MySQL数据库配置
+    fmis_mysql = ReadConfig().getDbConfig("test", "fmis", "mysql")
+
+    # 生产环境MySQL数据库配置
+    # fmis_mysql = ReadConfig().getDbConfig("release", "fmis", "mysql")
+
+if __name__ == '__main__':
+    s = Fmis_Common_Setting.fmis_mysql
+    print(s)
