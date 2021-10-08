ff1b53d4832d8c5886ee48afcd14935855204268
majiaqin <majiaqin@techb.top>
2021-10-08 09:43:23 +0800

    新增财务断言文件

---
+++ apps/Fmis/assert/assert_file.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: assert_file
+@time:2021/10/7
+@Author:majiaqin 170479
+@Desc:断言文件
+'''
+
+import unittest
+import assertpy
+
+class Fmis_Unit_Assert(unittest):
+    def __init__(self):
+        pass
+
---
+++ apps/utils/assert_utils.py
@@ -0,0 +1,6 @@
+'''
+@File: assert_utils
+@time:2021/10/7
+@Author:majiaqin 170479
+@Desc:
+'''
