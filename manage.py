4d93caaec96dcd3b5488b114c0dbb6ecfc31e71e
quanliu <13923832556@163.com>
2022-02-15 16:18:10 +0800

    新增人员登录

--- manage.py
+++ manage.py
@@ -6,14 +6,22 @@
 '''
 
 from apps import create_app
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 app = create_app("dev")
 
 # 接口调用入口
-@app.route("/projectEntry")
-def estoneInterfaceEntry():
-    return render_template("system_entry.html")
+@app.route("/projectEntry/<name>")
+def estoneInterfaceEntry(name):
+    return render_template("system_entry.html",username = name)
 
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if request.method=='POST':
+        user = request.form['name']
+        return redirect(url_for('estoneInterfaceEntry',name = user))
+    else:
+        user = request.args.get("name")
+        return redirect(url_for('estoneInterfaceEntry',name = user))
 
 if __name__ == '__main__':
     app.run(debug=True)
