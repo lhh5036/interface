d88912955b0eff43e1846f23dcd1f607848f1c1e
quanliu <13923832556@163.com>
2022-02-16 11:06:24 +0800

    新增登录和退出功能

--- manage.py
+++ manage.py
@@ -6,22 +6,39 @@
 '''
 
 from apps import create_app
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,session,flash
 app = create_app("dev")
 
 # 接口调用入口
-@app.route("/projectEntry/<name>")
-def estoneInterfaceEntry(name):
-    return render_template("system_entry.html",username = name)
+@app.route("/projectEntry")
+def estoneInterfaceEntry():
+    if 'username' in session:
+        username = session['username']
+        return render_template("system_entry.html",name = username)
+    return redirect(url_for('login'))
 
 @app.route('/login',methods=['POST','GET'])
 def login():
-    if request.method=='POST':
+    if request.method == 'POST':
         user = request.form['name']
-        return redirect(url_for('estoneInterfaceEntry',name = user))
+        if user == "test":
+            session['username'] = user
+            return redirect('projectEntry') # redirect直接是url，就是app.route的路径参数；url_for()是对函数进行操作
+        else:
+            return render_template('login.html')
     else:
         user = request.args.get("name")
-        return redirect(url_for('estoneInterfaceEntry',name = user))
+        if user == "test":
+            session['username'] = user
+            return redirect(url_for('estoneInterfaceEntry'))
+        else:
+            return render_template('login.html')
+
+
+@app.route('/logout',endpoint='logout',methods=['POST'])
+def logout():
+    session.clear()
+    return '666'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1",port='8888',debug=True)
