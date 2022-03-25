5a3969771fee877cb972173e841f00bc6ab9bfcd
majiaqin <majiaqin@techb.top>
2022-03-25 14:57:28 +0800

    新增用户模块

--- apps/UsermgtSystem/assert_usermgt/assert_file.py
+++ apps/UsermgtSystem/assert_usermgt/assert_file.py
@@ -4,5 +4,42 @@
 @File: assert_file
 @time:2022/3/25
 @Author:majiaqin 170479
-@Desc:
+@Desc:断言文件
 '''
+
+import requests
+import json
+import pprint
+
+from apps.utils.assert_utils import Assert_Api
+
+class Usermgt_Unit_Assert():
+    def __init__(self, response):
+        self.response = response
+
+    def usermgt_unit_assert(self, sql=None):
+        self.sql = sql
+        l = []
+        if self.sql == None:
+            statucode = Assert_Api(self.response).assert_statucode()
+            connect = Assert_Api(self.response).assert_connect()
+            l.append(statucode)
+            l.append(connect)
+            if "False" in l:
+                return "False"
+            else:
+                return "True"
+        else:
+            statucode = Assert_Api(self.response).assert_statucode()
+            connect = Assert_Api(self.response).assert_connect()
+            total = Assert_Api(self.response).assert_total(self.sql)
+            l.append(statucode)
+            l.append(connect)
+            l.append(total)
+            if "False" in l:
+                return "False"
+            else:
+                return "True"
+
+if __name__ == '__main__':
+    pass
--- apps/UsermgtSystem/usermgt_common_setting.py
+++ apps/UsermgtSystem/usermgt_common_setting.py
@@ -4,5 +4,7 @@
 @File: usermgt_common_setting
 @time:2022/3/25
 @Author:majiaqin 170479
-@Desc:
+@Desc:用户系统全局配置
 '''
+
+from apps.Common_Config.db_config import ReadConfig
