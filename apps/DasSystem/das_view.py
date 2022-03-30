de0ecc723a84933d208f537e51f11bb3124340ff
quanliu <13923832556@163.com>
2022-03-30 11:17:55 +0800

    update

--- apps/DasSystem/das_view.py
+++ apps/DasSystem/das_view.py
@@ -68,12 +68,12 @@ def run_dasTestcaseExecute():
     runner = bf(discover) # 实例化BeautifulReport模块
     runner.report(filename=filename,description='数据分析系统-接口自动化报告',report_dir=das_report_path)
     # 远程连接192.168.3.10服务器(需要先设置免密ssh-copy-id ip)
-    os.popen('ssh 192.168.3.10') # 远程连接
-    os.popen('rm -rf /data/test_file/result_das_*') # 删除远程192.168.3.10上面原来的报告
+    os.popen('ssh 192.168.3.10 "rm -rf /data/test_file/result_das_*"') # 远程连接,删除远程192.168.3.10上面原来的报告
     os.popen('exit') # 退出连接192.168.3.10
     os.popen('scp -r /home/InterfaceAutoTest/apps/DasSystem/report/{0}.html \
                  root@192.168.3.10:/data/test_file/'.format(filename)) # 远程传入最新的报告
     download_file_url = "http://192.168.3.10:81/test_file/{0}.html".format(filename)
-    msg = "数据分析测试用例报告地址:{0}".format(download_file_url)
+    msg = "数据分析测试用例报告地址" \
+          ":{0}".format(download_file_url)
     DingHelp(test_url,msg,["13923832556"],download_file_url).dinghelp()
     return "数据分析测试用例执行完成!"
