acfa144c4b085d53e26985643a37a961894d4043
majiaqin <majiaqin@techb.top>
2022-04-21 14:21:29 +0800

    新增新用户系统用例库

---
+++ apps/AllSystemData/UsermgtSystem/test/case/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: __init__.py
+@time:2022/4/21
+@Author:majiaqin 170479
+@Desc:新用户用例库
+'''
---
+++ apps/AllSystemData/UsermgtSystem/usermgt_api/usermgt_api_ware/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: __init__.py
+@time:2022/4/21
+@Author:majiaqin 170479
+@Desc:
+'''
