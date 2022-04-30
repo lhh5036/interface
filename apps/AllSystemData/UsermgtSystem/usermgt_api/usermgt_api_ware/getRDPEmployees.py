e4b438dc40b782516ac1c66be3af583b96bc92ae
majiaqin <majiaqin@techb.top>
2022-04-30 10:48:26 +0800

    断言增加assert_data方法

---
+++ apps/AllSystemData/UsermgtSystem/usermgt_api/usermgt_api_ware/getRDPEmployees.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: getRDPEmployees
+@time:2022/4/21
+@Author:majiaqin 170479
+@Desc:
+'''
--- apps/utils/assert_utils.py
+++ apps/utils/assert_utils.py
@@ -10,7 +10,9 @@
 import unittest
 import requests
 import json
+import pprint
 
+from deepdiff import DeepDiff, DeepSearch
 from apps.logger import MyLog
 from apps.utils.mySql_database_util import Mysql_handleOperator
 from apps.AllSystemData.FmisSystem.fmis_common_settting import Fmis_Common_Setting
@@ -22,12 +24,13 @@ logger = MyLog("AssertUtils").getlog()
 class Assert_Api(unittest.TestCase):
     '''获取返回数据供所有方法调用'''
     # 初始化父类
-    def __init__(self, response):
+    def __init__(self):
         super().__init__()
-        self.response = response
+
 
     '''断言返回数据状态码'''
-    def assert_statucode(self):
+    def assert_statucode(self, response):
+        self.response = response
         try:
             self.assertEqual(200, self.response.status_code)
             print(self.assertEqual(200, self.response.status_code))
@@ -39,7 +42,8 @@ class Assert_Api(unittest.TestCase):
             return "False"
 
     '''断言接口是否成功连接'''
-    def assert_connect(self):
+    def assert_connect(self, response):
+        self.response = response
         try:
             self.assertIn("connectionRefused", self.response.json())
             self.assertIn("success", self.response.json())
@@ -65,7 +69,8 @@ errorMsg的的值为{2}".format(self.response.json()["connectionRefused"],
             return "False"
 
     '''断言接口返回数据条目数是否正确'''
-    def assert_total(self, sql):
+    def assert_total(self, response, sql):
+        self.response = response
         # 查询数据条目数的sql
         self.sql = sql
         message = Json_Get(self.response.json()).json_get("total")
@@ -83,18 +88,94 @@ errorMsg的的值为{2}".format(self.response.json()["connectionRefused"],
             logger.error("Api response data not have total!")
             return "False"
 
+    '''
+    断言返回数据是否正确
+    (method_type为deepsearch校验json中的某个对象
+    为deepdiff校验整个json)
+    respon_data为返回数据的处理，需要处理为与check_data格式一致
+    check_data为数据库/其他数据源解析的数据
+    use_regexp当=True时可用正则表达式
+    当=False则不可用正则表达式
+    strict_checking当=True检查要匹配的对象的类型,比如'1234'!=1234
+    当=False则忽略匹配对象的类型,比如'1234'=1234
+    case_sensitive当=True区分大小写
+    当=False则忽略大小写
+    ignore_string_case当=True则可以不区分String大小写 
+    当=False则属于大小写敏感
+    '''
+    @classmethod
+    def assert_data(cls, respon_data, check_data,
+                    method_type='deepsearch',
+                    use_regexp=True,
+                    strict_checking=False,
+                    case_sensitive=True,
+                    ignore_string_case=False):
+        if method_type == 'deepsearch':
+            if type(check_data) == list:
+                assert_list = []
+                for dat in check_data:
+                    assert_msg = DeepSearch(respon_data, dat,
+                                            use_regexp=use_regexp,
+                                            strict_checking=strict_checking,
+                                            case_sensitive=case_sensitive)
+                    msg_dat = cls().deep_search_assert(assert_msg)
+                    assert_list.append(msg_dat)
+                if 'False' in assert_list:
+                    return 'False'
+                else:
+                    return 'True'
+            elif type(check_data) == dict:
+                assert_list = []
+                for dat in check_data.keys():
+                    assert_msg = DeepSearch(respon_data, check_data[dat],
+                                            use_regexp=use_regexp,
+                                            strict_checking=strict_checking,
+                                            case_sensitive=case_sensitive)
+                    msg_dat = cls().deep_search_assert(assert_msg)
+                    assert_list.append(msg_dat)
+                if 'False' in assert_list:
+                    return 'False'
+                else:
+                    return 'True'
+
+        elif method_type == 'deepdiff':
+            assert_msg = DeepDiff(respon_data, check_data,
+                                  view='text',
+                                  ignore_string_case=ignore_string_case)
+            if len(assert_msg) == 0:
+                logger.info("Api response data is true")
+                return "True"
+            else:
+                logger.error("Api response data is false")
+                return "False"
+
+    def deep_search_assert(self, assert_msg):
+        self.assert_msg = assert_msg
+        try:
+            if len(assert_msg['matched_values']) > 0:
+                logger.info("Api response data is true")
+                return "True"
+            elif len(assert_msg['matched_values']) == 0:
+                logger.error("Api response data is false")
+                return "False"
+        except KeyError:
+            logger.error("Api response data is false")
+            return "False"
 
 if __name__ == '__main__':
     def getRDPEmployees():
         url = 'http://192.168.3.165:8018/fmis/wishPlatformRefund/selectWishRefundDetail'
         header = {'Content-Type': 'application/json',
                   'Authorization': '5df26666b185fbf0b3437482125d340e'}
-        form = {"model":{"syncTime":"2021-09","accounts":["Bit by bit"],"sellers":[],"dataSource":2,"ids":[]},"current":1,"size":100}
+        form = {"model": {"syncTime": "2021-09",
+                          "accounts": ["Bit by bit"],
+                          "sellers": [], "dataSource": 2,
+                          "ids": []}, "current": 1, "size": 100}
 
         r = requests.post(url, headers=header, data=json.dumps(form))
         return r
     response = getRDPEmployees()
-    # pprint.pprint(response.json())
-    s = Assert_Api(response).assert_total(1)
-    print(s)
+    pprint.pprint(response.json())
+    # s = Assert_Api(response).assert_data(1)
+    # print(s)
     pass
