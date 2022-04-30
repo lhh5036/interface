b85eb25ac638acad7c690df6f43a1d9c73cc182c
majiaqin <majiaqin@techb.top>
2022-04-30 14:04:39 +0800

    新增getRDPEmployees接口文件

--- apps/AllSystemData/UsermgtSystem/usermgt_api/usermgt_api_ware/getRDPEmployees.py
+++ apps/AllSystemData/UsermgtSystem/usermgt_api/usermgt_api_ware/getRDPEmployees.py
@@ -4,5 +4,32 @@
 @File: getRDPEmployees
 @time:2022/4/21
 @Author:majiaqin 170479
-@Desc:
+@Desc:查看账号在该页面下的数据权限
 '''
+import requests
+import json
+import pprint
+from apps.Common_Config.interface_common_info import Common_TokenHeader
+from apps.AllSystemData.UsermgtSystem.usermgt_api.usermgtSystem_interface_param import UsermgtApiInputParam
+from apps.AllSystemData.UsermgtSystem.usermgt_api.usermgtSystem_interface_url import UsermgtApiUrl
+from apps.Common_Config.operate_api_data import api_assemble
+from apps.logger import MyLog
+
+
+# 实例化日志类
+logger = MyLog("GetRDPEmployeesApi").getlog()
+
+class GetRDPEmployeesApi():
+    def __init__(self, api_url):
+        self.api_url = api_url
+        pass
+
+    @api_assemble(UsermgtApiUrl.getRDPEmployees_url)
+    def getRDPEmployees(self, params):
+        self.params = params
+        logger.info("getRDPEmployees ---->start!")
+        return self.params
+
+
+if __name__ == '__main__':
+    pass
