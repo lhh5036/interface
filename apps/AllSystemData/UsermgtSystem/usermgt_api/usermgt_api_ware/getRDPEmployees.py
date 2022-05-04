f9ac6e2a1cd4218155cf90780b63e08fe779aabd
majiaqin <majiaqin@techb.top>
2022-05-04 14:25:50 +0800

    新增接口文件-getRDPEmployees

--- apps/AllSystemData/UsermgtSystem/usermgt_api/usermgt_api_ware/getRDPEmployees.py
+++ apps/AllSystemData/UsermgtSystem/usermgt_api/usermgt_api_ware/getRDPEmployees.py
@@ -8,21 +8,37 @@
 '''
 from apps.AllSystemData.UsermgtSystem.usermgt_api.usermgtSystem_interface_url import UsermgtApiUrl
 from apps.Common_Config.operate_api_data import api_assemble
-from apps.logger import MyLog
+from apps.AllSystemData.UsermgtSystem.usermgt_common_setting import Usermgt_Common_Setting
+from apps.utils.mySql_database_util import get_data, convert_list
 
-# 实例化日志类
-logger = MyLog("GetRDPEmployeesApi").getlog()
 
 class GetRDPEmployeesApi():
-    def __init__(self, api_url):
-        self.api_url = api_url
+    def __init__(self):
         pass
 
-    @api_assemble(UsermgtApiUrl.getRDPEmployees_url)
-    def getRDPEmployees(self, params):
-        self.params = params
-        logger.info("getRDPEmployees ---->start!")
-        return self.params
+    '''数据库获取员工账号'''
+    @convert_list
+    @get_data(Usermgt_Common_Setting.usermgt_mysql)
+    def get_employeeNo(self):
+        sql = "SELECT employee_no FROM employee LIMIT 10;"
+        return sql
+
+    '''数据库获取菜单code'''
+    @convert_list
+    @get_data(Usermgt_Common_Setting.usermgt_mysql)
+    def get_menuCode(self):
+        sql = "SELECT code FROM menu LIMIT 10;"
+        return sql
+
+    @classmethod
+    def splicing_params_list(cls):
+        get_employeeNo_list = cls().get_employeeNo()
+        get_menuCode_list = cls().get_menuCode()
+        params_list = []
+        for i in range(len(get_employeeNo_list)):
+            params_list.append([get_employeeNo_list[i],
+                                get_menuCode_list[i]])
+        return params_list
 
 
 if __name__ == '__main__':
