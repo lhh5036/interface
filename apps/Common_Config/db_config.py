e01cd2c9c0d180972365423e9de2ba9c17b24185
majiaqin <majiaqin@techb.top>
2022-05-09 17:37:08 +0800

    重构redis类

--- apps/Common_Config/db_config.py
+++ apps/Common_Config/db_config.py
@@ -12,7 +12,7 @@ import redis
 from rediscluster import RedisCluster
 
 '''解析mysql_sources_config文件中MYSQL配置'''
-def parseMySqlFile(cf,databasename):
+def parseMySqlFile(cf, databasename):
     _host = cf.get(databasename, "mysql_host")
     _user = cf.get(databasename, "mysql_user")
     _password = cf.get(databasename, "mysql_password")
@@ -20,7 +20,7 @@ def parseMySqlFile(cf,databasename):
     return [_host, _user, _password, _database]
 
 '''解析es_sources_config.ini文件中ES配置'''
-def parseEsFile(cf,databasename):
+def parseEsFile(cf, databasename):
     _host = cf.get(databasename, "es_host")
     _port = cf.get(databasename, "es_port")
     _timeout = int(cf.get(databasename, "es_timeout"))
@@ -61,81 +61,6 @@ def Mysql_Db_Config(env):
 def Es_Db_Config(env):
     es_config = Get_Db_Info().get_esdb_info(env)
     return [es_config[i] for i in range(len(es_config))]
-
-'''
-redis调用类
-'''
-# redis生产环境集群
-nodes_release = [{"host": "10.100.1.91", "port": 6379},
-                 {"host": "10.100.1.93", "port": 6379},
-                 {"host": "10.100.1.95", "port": 6379}]
-
-# redis测试环境集群
-nodes_test = [{"host": "192.168.3.5", "port": 8001,
-               "user": "redis_admin",
-               "password": "halfj$$1/2red"},
-              {"host": "192.168.3.5", "port": 8002,
-               "user": "redis_admin",
-               "password": "halfj$$1/2red"},
-              {"host": "192.168.3.5", "port": 8003,
-               "user": "redis_admin",
-               "password": "halfj$$1/2red"},
-              {"host": "192.168.3.6", "port": 8004,
-               "user": "redis_admin",
-               "password": "halfj$$1/2red"},
-              {"host": "192.168.3.6", "port": 8005,
-               "user": "redis_admin",
-               "password": "halfj$$1/2red"},
-              {"host": "192.168.3.6", "port": 8006,
-               "user": "redis_admin",
-               "password": "halfj$$1/2red"}]
-
-# redis连接
-class Redis_User():
-    def __init__(self, host, port=6379, decode_responses=True):
-        self._host = host
-        self._port = port
-        self._decode_responses = decode_responses
-
-    # 取出键对应的值(单节点)
-    def redis_get(self, keys):
-        self._keys = keys
-        # 创建连接的进程池
-        poll = redis.ConnectionPool(host=self._host, port=self._port)
-        # 选择需要连接的进程池
-        r = redis.Redis(connection_pool=poll)
-        get_value = r.get(self._keys)
-        return get_value
-
-    # 取出键对应的值(集群)
-    def redis_cluster(self, nodes, keys):           # nodes为redis集群
-        self._nodes = nodes
-        self._keys = keys
-        r = RedisCluster(startup_nodes=self._nodes, decode_responses=self._decode_responses)
-        get_values = r.get(self._keys)
-        return get_values
-
-    def redis_zscore(self, nodes, name, value):     # nodes为redis集群
-        self._nodes = nodes
-        self._name = name
-        self._value = value
-        r = RedisCluster(startup_nodes=self._nodes, decode_responses=self._decode_responses)
-        get_values = r.zscore(self._name, self._value)
-        return get_values
-
-# 解析redis_sources_config文件中REDIS配置
-class parseRedisFile():
-    def __init__(self, cf, databasename):
-        self.cf = cf
-        self.databasename = databasename
-
-    def host(self):
-        _host = self.cf.get(self.databasename, "redis_host")
-        return _host
-    def port(self):
-        _port = self.cf.get(self.databasename, "redis_port")
-        return _port
-
 
 if __name__ == '__main__':
     pass
--- apps/utils/redis_database_util.py
+++ apps/utils/redis_database_util.py
@@ -1,45 +1,90 @@
-'''
-@File: es_handleOperator.py
-@time:2021/8/19
-@Author:quanliu
-@Desc:Redis数据库操作工具类
-'''
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
 
 import redis
+import itertools
 from rediscluster import RedisCluster
 
+nodes_release = [{"host": "10.100.1.181", "port": 6379},
+                 {"host": "10.100.1.182", "port": 6379},
+                 {"host": "10.100.1.183", "port": 6379},
+                 {"host": "10.100.1.184", "port": 6379},
+                 {"host": "10.100.1.185", "port": 6379},
+                 {"host": "10.100.1.186", "port": 6379}]
+nodes_release_info = {"username": "readonly", "password": "!QAZxsw2"}
+
+nodes_test = [{"host": "192.168.3.5", "port": 8001},
+              {"host": "192.168.3.5", "port": 8002},
+              {"host": "192.168.3.5", "port": 8003},
+              {"host": "192.168.3.6", "port": 8004},
+              {"host": "192.168.3.6", "port": 8005},
+              {"host": "192.168.3.6", "port": 8006}]
+nodes_test_info = {"username": "redis_admin", "password": "halfj$$1/2red"}
+
 class Redis_User():
-    def __init__(self, host, port=6379, decode_responses=True):
-        self.host = host
-        self.port = port
+    def __init__(self, nodes, username='redis_admin',
+                 password='halfj$$1/2red', decode_responses=True):
+        self.nodes = nodes
+        self.username = username
+        self.password = password
         self.decode_responses = decode_responses
 
-    # 取出键对应的值(单节点)
-    def redis_get(self, keys):
-        self.keys = keys
-        # 创建连接的进程池
-        poll = redis.ConnectionPool(host=self.host, port=self.port)
-        # 选择需要连接的进程池
-        r = redis.Redis(connection_pool=poll)
-        get_value = r.get(self.keys)
-        return get_value
-
-    # 取出键对应的值(集群)
-    def redis_cluster(self, nodes, keys):
-        self.nodes = nodes
-        self.keys = keys
-        self.nodes = nodes
-        r = RedisCluster(startup_nodes=self.nodes, decode_responses=self.decode_responses)
-        get_values = r.get(self.keys)
-        return get_values
-
-    def redis_zscore(self, nodes, name, value):
-        self.nodes = nodes
+    # 对节点redis进行增删改查
+    def redis_operate(self, method='get', key=None, name=None, value=None):
+        self.key = key
         self.name = name
         self.value = value
-        r = RedisCluster(startup_nodes=self.nodes, decode_responses=self.decode_responses)
-        get_values = r.zscore(self.name, self.value)
-        return get_values
+        self.method = method
+        redis_value_lis = []
+        for node in self.nodes:
+            # 连接redis
+            redis_conn = redis.StrictRedis(host=node['host'],
+                                           port=node['port'],
+                                           username=self.username,
+                                           password=self.password,
+                                           decode_responses=self.decode_responses)
+            if self.method == 'get':
+                try:
+                    return redis_conn.get(self.key)
+                except:
+                    continue
+            elif self.method == 'hget':
+                try:
+                    return redis_conn.hget(self.name, self.key)
+                except:
+                    continue
+            elif self.method == 'set':
+                try:
+                    return redis_conn.set(self.name, self.value)
+                except:
+                    continue
+            elif self.method == 'hset':
+                try:
+                    return redis_conn.hset(self.name, self.key, self.value)
+                except:
+                    continue
+            elif self.method == 'del':
+                try:
+                    return redis_conn.delete(self.name)
+                except:
+                    continue
+            elif self.method == 'hdel':
+                try:
+                    return redis_conn.hdel(self.name, self.key)
+                except:
+                    continue
+            elif self.method == 'keys':
+                redis_value = redis_conn.keys(self.key)
+                if redis_value != None:
+                    redis_value_lis.append(redis_value)
+        redis_value_list = [i for i in itertools.chain(*redis_value_lis)]
+        return redis_value_list
+
+
 
 if __name__ == '__main__':
-    pass
+    values = Redis_User(nodes_test, username=nodes_test_info['username'],
+                        password=nodes_test_info['password']).redis_operate(name='redis:demo',
+                                                                            key='170479',
+                                                                            method='hdel')
+    print(values)
