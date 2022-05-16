7a885259e408131677500df3b1f9839837b28c7a
majiaqin <majiaqin@techb.top>
2022-05-16 16:38:15 +0800

    新增getEmployeeList接口入参数据准备

--- apps/AllSystemData/UsermgtSystem/usermgt_api/usermgt_api_ware/getEmployeeList.py
+++ apps/AllSystemData/UsermgtSystem/usermgt_api/usermgt_api_ware/getEmployeeList.py
@@ -4,5 +4,30 @@
 @File: getEmployeeList
 @time:2022/5/16
 @Author:majiaqin 170479
-@Desc:
+@Desc:根据职位查询员工
 '''
+import pprint
+from apps.utils.mySql_database_util import get_data, convert_list
+from apps.AllSystemData.UsermgtSystem.usermgt_common_setting import Usermgt_Common_Setting
+
+class GetEmployeeList():
+    def __init__(self):
+        pass
+
+    '''获取职位名称'''
+    @convert_list
+    @get_data(Usermgt_Common_Setting.usermgt_mysql)
+    def get_position_name(self):
+        sql = "SELECT position_name FROM employee \
+WHERE position_name IS NOT NULL \
+LIMIT 10;"
+        return sql
+
+    @classmethod
+    def splicing_params_list(cls):
+        params_list = cls().get_position_name()
+        return params_list
+
+if __name__ == '__main__':
+    pprint.pprint(GetEmployeeList().splicing_params_list())
+    pass
