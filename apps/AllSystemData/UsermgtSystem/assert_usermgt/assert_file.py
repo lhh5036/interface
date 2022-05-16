dfed3708e011a45e863c7978aad7519e47b6c45d
majiaqin <majiaqin@techb.top>
2022-05-16 10:41:45 +0800

    修改新用户断言文件

--- apps/AllSystemData/UsermgtSystem/assert_usermgt/assert_file.py
+++ apps/AllSystemData/UsermgtSystem/assert_usermgt/assert_file.py
@@ -18,27 +18,37 @@ def usermgt_unit_assert(sql=None):
         def demo(*args):
             l = []
             if sql == None:
-                statucode = Assert_Api().assert_statucode(func(*args))
-                connect = Assert_Api().assert_connect(func(*args))
+                statucode = Assert_Api().assert_statucode(func(*args)[0])
+                connect = Assert_Api().assert_connect(func(*args)[0], func(*args)[1])
                 l.append(statucode)
                 l.append(connect)
                 if "False" in l:
+                    print("False")
                     return "False"
                 else:
+                    print("True")
                     return "True"
             else:
-                statucode = Assert_Api().assert_statucode(func(*args))
-                connect = Assert_Api().assert_connect(func(*args))
-                total = Assert_Api().assert_total(func(*args), sql)
+                statucode = Assert_Api().assert_statucode(func(*args)[0])
+                connect = Assert_Api().assert_connect(func(*args)[0], func(*args)[1])
+                total = Assert_Api().assert_total(func(*args)[1], sql)
                 l.append(statucode)
                 l.append(connect)
                 l.append(total)
                 if "False" in l:
+                    print("False")
                     return "False"
                 else:
+                    print("True")
                     return "True"
         return demo
     return wragger
 
+@usermgt_unit_assert()
+def test():
+    l = [200, {'success': True, 'result': None, 'errorMsg': None, 'connectionRefused': False}]
+    return l
+
 if __name__ == '__main__':
+    test()
     pass
