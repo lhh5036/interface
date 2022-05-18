24cc49a09d8b61424ae82e9ce6b3726fd4bb47d4
majiaqin <majiaqin@techb.top>
2022-05-18 09:44:55 +0800

    新增listTeamLeaderByEmployeeNo接口测试用例

---
+++ apps/AllSystemData/UsermgtSystem/test/case/test_listTeamLeaderByEmployeeNo.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: test_listTeamLeaderByEmployeeNo
+@time:2022/5/18
+@Author:majiaqin 170479
+@Desc:listTeamLeaderByEmployeeNo接口测试用例
+'''
+
+import unittest
+import random
+from ddt import ddt, data, unpack
+from apps.AllSystemData.UsermgtSystem.usermgt_api.usermgtSystem_interface_url import UsermgtApiUrl
+from apps.AllSystemData.UsermgtSystem.usermgt_api.usermgt_api_ware.listTeamLeaderByEmployeeNo import ListTeamLeaderByEmployeeNoApi
+from apps.AllSystemData.UsermgtSystem.usermgt_api.usermgtSystem_interface_param import UsermgtApiInputParam
+from apps.Common_Config.operate_api_data import splicing_params_new, api_assemble
+from apps.AllSystemData.UsermgtSystem.assert_usermgt.assert_file import usermgt_unit_assert
+from apps.logger import MyLog
+# 实例化日志类
+logger = MyLog("ListTeamLeaderByEmployeeNoApi").getlog()
+
+class Test_listTeamLeaderByEmployeeNo(unittest.TestCase):
+    @usermgt_unit_assert()
+    @api_assemble(UsermgtApiUrl.listTeamLeaderByEmployeeNo_url)
+    @splicing_params_new()
+    def testCase(self):
+        '''判断员工是否为负责人,返回相关员工信息'''
+        params_list = ListTeamLeaderByEmployeeNoApi().splicing_params_list()
+        params = params_list[random.randint(0, len(params_list)-1)]
+        lis = []
+        lis.append([UsermgtApiInputParam.listTeamLeaderByEmployeeNo_param02,
+                    UsermgtApiInputParam.listTeamLeaderByEmployeeNo_param01])
+        lis.append({"employeeNo": params[0],
+                    "servicePlatform": params[1]})
+        logger.info(lis[1])
+        return lis
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+    pass
--- apps/AllSystemData/UsermgtSystem/usermgt_api/usermgt_api_ware/listTeamLeaderByEmployeeNo.py
+++ apps/AllSystemData/UsermgtSystem/usermgt_api/usermgt_api_ware/listTeamLeaderByEmployeeNo.py
@@ -4,5 +4,31 @@
 @File: listTeamLeaderByEmployeeNo
 @time:2022/5/17
 @Author:majiaqin 170479
-@Desc:
+@Desc:判断员工是否为负责人,返回相关员工信息
 '''
+
+import pprint
+from apps.AllSystemData.UsermgtSystem.usermgt_common_setting import Usermgt_Common_Setting
+from apps.utils.mySql_database_util import get_data, convert_list
+
+class ListTeamLeaderByEmployeeNoApi():
+    def __init__(self):
+        pass
+
+    @get_data(Usermgt_Common_Setting.usermgt_mysql)
+    def get_data(self):
+        sql = "SELECT employee_no, service_platform \
+FROM employee \
+WHERE service_platform IS NOT NULL \
+GROUP BY service_platform \
+LIMIT 10;"
+        return sql
+
+    @classmethod
+    def splicing_params_list(cls):
+        params_list = list(cls().get_data())
+        return params_list
+
+if __name__ == '__main__':
+    pprint.pprint(ListTeamLeaderByEmployeeNoApi().splicing_params_list())
+    pass
