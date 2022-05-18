50672067d944a2c0ea5f167cb704f3b5e6e96618
majiaqin <majiaqin@techb.top>
2022-05-18 10:37:00 +0800

    新增newListOrgStructAndEmployeeVO接口测试用例

---
+++ apps/AllSystemData/UsermgtSystem/test/case/test_newListOrgStructAndEmployeeVO.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: test_newListOrgStructAndEmployeeVO
+@time:2022/5/18
+@Author:majiaqin 170479
+@Desc:newListOrgStructAndEmployeeVO接口测试用例
+'''
+
+import unittest
+import random
+from ddt import ddt, data, unpack
+from apps.AllSystemData.UsermgtSystem.usermgt_api.usermgtSystem_interface_url import UsermgtApiUrl
+from apps.AllSystemData.UsermgtSystem.usermgt_api.usermgt_api_ware.newListOrgStructAndEmployeeVO import NewListOrgStructAndEmployeeVOApi
+from apps.AllSystemData.UsermgtSystem.usermgt_api.usermgtSystem_interface_param import UsermgtApiInputParam
+from apps.Common_Config.operate_api_data import splicing_params_new, api_assemble
+from apps.AllSystemData.UsermgtSystem.assert_usermgt.assert_file import usermgt_unit_assert
+from apps.logger import MyLog
+# 实例化日志类
+logger = MyLog("ListOrgStructAndEmployeeVOApi").getlog()
+
+class Test_newListOrgStructAndEmployeeVO():
+    @usermgt_unit_assert()
+    @api_assemble(UsermgtApiUrl.listNextEmployeeByDeptId_url)
+    @splicing_params_new()
+    def testCase(self):
+        '''根据平台和工号获取级联获取员工信息'''
+        params_list = NewListOrgStructAndEmployeeVOApi().splicing_params_list()
+        params = params_list[random.randint(0, len(params_list)-1)]
+        lis = []
+        lis.append([UsermgtApiInputParam.newListOrgStructAndEmployeeVO_param01])
+        lis.append({"orgName": params[0], "employeeNo": params[1]})
+        logger.info(lis[1])
+        return lis
+        pass
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+    pass
---
+++ apps/AllSystemData/UsermgtSystem/usermgt_api/usermgt_api_ware/newListOrgStructAndEmployeeVO.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: newListOrgStructAndEmployeeVO
+@time:2022/5/18
+@Author:majiaqin 170479
+@Desc:根据平台和工号获取级联获取员工信息
+'''
+import pprint
+from apps.AllSystemData.UsermgtSystem.usermgt_common_setting import Usermgt_Common_Setting
+from apps.utils.mySql_database_util import get_data, convert_list
+
+class NewListOrgStructAndEmployeeVOApi():
+    def __init__(self):
+        pass
+
+    @get_data(Usermgt_Common_Setting.usermgt_mysql)
+    def get_data(self):
+        sql = "SELECT o.org_name, e.employee_no \
+FROM employee e \
+LEFT JOIN org_struct o \
+ON o.id = e.dept_id \
+WHERE o.org_name IS NOT NULL \
+GROUP BY o.org_name \
+LIMIT 10;"
+        return sql
+
+    @classmethod
+    def splicing_params_list(cls):
+        params_list = list(cls().get_data())
+        return params_list
+
+if __name__ == '__main__':
+    pprint.pprint(NewListOrgStructAndEmployeeVOApi().splicing_params_list())
+    pass
