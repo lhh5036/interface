343e1dc2624f1714b11f0bef4bc71a5560ec9ec8
majiaqin <majiaqin@techb.top>
2022-05-25 09:05:15 +0800

    新增ignore文件

---
+++ apps/AllSystemData/UsermgtSystem/test/case/test_queryRoleByEmpNos.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: test_queryRoleByEmpNos
+@time:2022/5/19
+@Author:majiaqin 170479
+@Desc:
+'''
---
+++ apps/AllSystemData/UsermgtSystem/usermgt_api/usermgt_api_ware/queryRoleByEmpNos.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: queryRoleByEmpNos
+@time:2022/5/19
+@Author:majiaqin 170479
+@Desc:
+'''
