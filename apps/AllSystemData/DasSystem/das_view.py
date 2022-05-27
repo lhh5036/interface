bf883eaa8b3a1ce23d68abc9507e2bc5583d9b07
quanliu <13923832556@163.com>
2022-05-27 15:01:26 +0800

    updaet

--- apps/AllSystemData/DasSystem/das_view.py
+++ apps/AllSystemData/DasSystem/das_view.py
@@ -51,32 +51,33 @@ def run_dasTestcaseExecute():
     # runner.run(discover)
     # fp.close()
     # 方法二
-    # filename = "result_"+now
+    # filename = "result_das_"+now
     # runner = bf(discover) # 实例化BeautifulReport模块
     # runner.report(filename=filename,description='数据分析系统-接口自动化报告',report_dir=das_report_path)
     # 添加浏览器驱动
+    # global driver
     # driver = webdriver.Chrome()
     # driver.maximize_window()
     # driver.get(report_abspath)
     # return "数据分析测试用例执行完成!"
     # 方法三
-    # runner = TestRunner(discover)
-    # runner.run() # 执行用例
-    # # 发送钉钉通知，可以得到执行用例成功个数、失败个数、总数
-    # runner.dingtalk_notice(url=test_url)
-    # return "数据分析测试用例执行完成!"
+    runner = TestRunner(discover)
+    runner.run() # 执行用例
+    # 发送钉钉通知，可以得到执行用例成功个数、失败个数、总数
+    runner.dingtalk_notice(url=test_url)
+    return "数据分析测试用例执行完成!"
     # 方法四--推送钉钉报告连接
-    filename = "result_das_"+now + ".html"
-    runner = bf(discover) # 实例化BeautifulReport模块
-    runner.report(filename=filename,description='数据分析系统-接口自动化报告',report_dir=das_report_path)
-    # 远程连接192.168.3.10服务器(需要先设置免密ssh-copy-id ip)
-    os.popen('ssh 192.168.3.10 "rm -rf /data/interfaceAutoTest_file/result_das_*"') # 远程连接,删除远程192.168.3.10上面原来的报告
-    os.popen('scp -r /home/InterfaceAutoTest/apps/AllSystemData/DasSystem/report/{0} \
-                 root@192.168.3.10:/data/interfaceAutoTest_file/'.format(filename)) # 远程传入最新的报告
-    download_file_url = "http://192.168.3.10:81/interfaceAutoTest_file/{0}".format(filename)
-    das_url = urlparse(download_file_url).geturl()
-    msg = "数据分析测试报告地址:{0}".format(das_url)
-    # DingHelp(test_url,msg,["13923832556"]).dinghelp() # 推送钉钉消息
-    return render_template("system_report.html",
-                           das_report_url=das_url,
-                           urlname='das')
+    # filename = "result_das_"+now + ".html"
+    # runner = bf(discover) # 实例化BeautifulReport模块
+    # runner.report(filename=filename,description='数据分析系统-接口自动化报告',report_dir=das_report_path)
+    # # 远程连接192.168.3.10服务器(需要先设置免密ssh-copy-id ip)
+    # os.popen('ssh 192.168.3.10 "rm -rf /data/interfaceAutoTest_file/result_das_*"') # 远程连接,删除远程192.168.3.10上面原来的报告
+    # os.popen('scp -r /home/InterfaceAutoTest/apps/AllSystemData/DasSystem/report/{0} \
+    #              root@192.168.3.10:/data/interfaceAutoTest_file/'.format(filename)) # 远程传入最新的报告
+    # download_file_url = "http://192.168.3.10:81/interfaceAutoTest_file/{0}".format(filename)
+    # das_url = urlparse(download_file_url).geturl()
+    # msg = "数据分析测试报告地址:{0}".format(das_url)
+    # # DingHelp(test_url,msg,["13923832556"]).dinghelp() # 推送钉钉消息
+    # return render_template("system_report.html",
+    #                        das_report_url=das_url,
+    #                        urlname='das')
