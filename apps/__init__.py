d28c39c03772638ba35f1bb35e3ed3a3aa78d851
quanliu <13923832556@163.com>
2022-05-30 14:04:49 +0800

    updaet

--- apps/__init__.py
+++ apps/__init__.py
@@ -34,27 +34,19 @@ def create_app(config_name):
     app.permanent_session_lifetime = 1551   # session的生存时间——测试时设置
 
     # 在Flask对象中注册蓝图模块中的蓝图对象 das_view 中的 das_api
-    app.register_blueprint(das_view.das_api,
-                           url_prefix="/interfaceTest/das/")
+    app.register_blueprint(das_view.das_api,url_prefix="/interfaceTest/das/")
     # 在Flask对象中注册蓝图模块中的蓝图对象 fmis_view 中的 fmis_api
-    app.register_blueprint(fmis_view.fmis_api,
-                           url_prefix="/interfaceTest/fmis/")
+    app.register_blueprint(fmis_view.fmis_api, url_prefix="/interfaceTest/fmis/")
     # 在Flask对象中注册蓝图模块中的蓝图对象 usermgt_view 中的 usermgt_api
-    app.register_blueprint(usermgt_view.usermgt_api,
-                           url_prefix="/interfaceTest/usermgt/")
+    app.register_blueprint(usermgt_view.usermgt_api,url_prefix="/interfaceTest/usermgt/")
     # 订单系统
-    app.register_blueprint(sale_view.sale_api,
-                           url_prefix="/interfaceTest/sale/")
+    app.register_blueprint(sale_view.sale_api, url_prefix="/interfaceTest/sale/")
     # 物流系统
-    app.register_blueprint(tms_view.tms_api,
-                           url_prefix="/interfaceTest/tms/")
+    app.register_blueprint(tms_view.tms_api, url_prefix="/interfaceTest/tms/")
     # 产品系统
-    app.register_blueprint(product_view.product_api,
-                           url_prefix="/interfaceTest/product/")
+    app.register_blueprint(product_view.product_api, url_prefix="/interfaceTest/product/")
     # 采购系统
-    app.register_blueprint(pms_view.pms_api,
-                           url_prefix="/interfaceTest/pms/")
+    app.register_blueprint(pms_view.pms_api,url_prefix="/interfaceTest/pms/")
     # 仓库系统
-    app.register_blueprint(wms_view.wms_api,
-                           url_prefix="/interfaceTest/wms/")
+    app.register_blueprint(wms_view.wms_api,url_prefix="/interfaceTest/wms/")
     return app
