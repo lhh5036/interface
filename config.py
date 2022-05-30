91081d61e8a426a46c27260aac5654f1670ef4da
quanliu <13923832556@163.com>
2022-05-30 15:58:24 +0800

    update

--- config.py
+++ config.py
@@ -4,6 +4,7 @@
 @Author:quanliu
 @Desc:配置文件
 '''
+import logging
 
 class Config(object):
     DEBUG = False
@@ -11,6 +12,7 @@ class Config(object):
 
 class DevelopementConfig(Config):
     DEBUG = True
+    LOG_LEVEL = logging.DEBUG
     host = "192.168.3.180" # 测试环境
     port = "3306"
     username = "root"
@@ -21,12 +23,18 @@ class DevelopementConfig(Config):
     DB_URI = "mysql+pymysql://{username}:{password}@{host}:{port}/{databasename}?charset=utf8mb4" \
         .format(username=username, password=password, host=host, port=port, databasename=databasename)
     SQLALCHEMY_DATABASE_URI = DB_URI
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_POOL_TIMEOUT = 20
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # 如果设置成 True (默认情况)，Flask-SQLAlchemy 将会追踪对象的修改并且发送信号。这需要额外的内存， 如果不必要的可以禁用它。
+    SQLALCHEMY_POOL_TIMEOUT = 20 # 连接超时时间
+    SQLALCHEMY_POOL_SIZE = 5 # 数据库池的大小，默认为5
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProductionConfig(Config):
     TESTING = True
+    LOG_LEVEL = logging.ERROR
     host = "192.168.3.180"
     port = "3306"
     username = "root"
