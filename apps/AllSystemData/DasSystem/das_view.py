996717a7d8994c413b33003aad70523a03f45589
quanliu <13923832556@163.com>
2022-05-30 16:22:50 +0800

    update

--- apps/AllSystemData/DasSystem/das_view.py
+++ apps/AllSystemData/DasSystem/das_view.py
@@ -24,8 +24,8 @@ from urllib.parse import urlparse
 test_url = WebHook.test_url
 das_api = Blueprint("das_api",__name__) # 实例化一个蓝图(Blueprint)对象
 
-das_garder_path = os.path.dirname(os.path.realpath(__file__)) + "\\test\\case\\" # 获取数据分析测试用例文件路径
-das_report_path = os.path.dirname(os.path.realpath(__file__)) + "\\report" # 测试数据分析报告路径
+das_garder_path = os.path.dirname(os.path.realpath(__file__)) + "/test/case/" # 获取数据分析测试用例文件路径
+das_report_path = os.path.dirname(os.path.realpath(__file__)) + "/report" # 测试数据分析报告路径
 
 # 为创建的蓝图添加路由配置
 @das_api.route('/allTestCase/execute')
