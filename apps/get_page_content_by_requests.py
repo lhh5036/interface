1c916fe8607c5827a0f7062b8ceafed13e206e88
quanliu <13923832556@163.com>
2022-05-30 11:49:15 +0800

    update

---
+++ apps/get_page_content_by_requests.py
@@ -0,0 +1,20 @@
+'''
+@File: get_page_content_by_requests.py
+@time:2022/5/30
+@Author:
+@Desc:
+'''
+import requests,json
+
+def get_page_content_by_requests(url,headers,inParam,methods="post",timeout=5000):
+    try:
+        if methods == "get":
+            resp = requests.get(url=url, headers=headers, params=json.dumps(inParam), timeout=timeout)
+            return resp
+        if methods == "post":
+            resp = requests.post(url=url,headers=headers,data=json.dumps(inParam),timeout=timeout)
+            return resp
+    except Exception as e:
+        return Exception, e
+
+
