b7ff8fc2eb3f2dfccd104360245f141f7f02163a
quanliu <13923832556@163.com>
2022-05-31 14:04:55 +0800

    update

--- apps/__init__.py
+++ apps/__init__.py
@@ -11,27 +11,17 @@ from apps.AllSystemData.SaleSystem import sale_view
 from apps.AllSystemData.TmsSystem import tms_view
 from apps.AllSystemData.UsermgtSystem import usermgt_view
 from apps.AllSystemData.WmsSystem import wms_view
-from config import DevelopementConfig, ProductionConfig
 from logger import setup_log
+from config import config
 
-config = {
-    "dev": DevelopementConfig,
-    "prop": ProductionConfig,
-}
 
 def create_app(config_name):
     """项目的初始化函数"""
     app = Flask(__name__, template_folder='templates',
                 static_folder='static')
-    # 设置配置类
-    Config = config[config_name]
     # 加载配置
-    app.config.from_object(Config)
-    app.logger.addHandler(setup_log(Config))
-    app.config['JSON_AS_ASCII'] = False
-    # 为session加密的key
-    app.config['SECRET_KEY'] = "3422sfsdfsdw4523gdgdsfs" # secret_key设置成os.urandom(24)的话，它的值就会变化，而一旦发生变化，
-    # 原来的cookie中的token就不能被新的secret_key验证，于是cookie就失效了，相应的session存的内容也就没了，所以会再次提示用户登录。因此以后secret_key最好设置成一个固定的字符串！
+    app.config.from_object(config[config_name])
+    app.logger.addHandler(setup_log(config[config_name]))
     app.permanent_session_lifetime = 1551   # session的生存时间——测试时设置
 
     # 在Flask对象中注册蓝图模块中的蓝图对象 das_view 中的 das_api
--- config.py
+++ config.py
@@ -1,17 +1,25 @@
 '''
-@File: config.py
+@File: settings.py
 @time:2021/9/10
 @Author:quanliu
 @Desc:配置文件
 '''
 
-class Config(object):
+class BaseConfig():
     DEBUG = False
     TESTING = False
 
-class DevelopementConfig(Config):
-    DEBUG = False
+class DevelopementConfig(BaseConfig):
+    ENV = "development"
+    DEBUG = True
+    LOG_LEVEL = False
+    JSON_AS_ASCII = True
+    # 为session加密的key
+    # secret_key设置成os.urandom(24)的话，它的值就会变化，而一旦发生变化，
+    # 原来的cookie中的token就不能被新的secret_key验证，于是cookie就失效了，相应的session存的内容也就没了，所以会再次提示用户登录。因此以后secret_key最好设置成一个固定的字符串！
+    SECRET_KEY = "3422sfsdfsdw4523gdgdsfs"
 
 
-class ProductionConfig(Config):
+class ProductionConfig(BaseConfig):
+    ENV = "production"
     TESTING = True
