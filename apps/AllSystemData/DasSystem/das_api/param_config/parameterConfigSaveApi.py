d21890bdc3b5c326a8001d5acd504796712635f9
quanliu <13923832556@163.com>
2022-05-31 14:21:19 +0800

    更新日志

--- apps/AllSystemData/DasSystem/das_api/param_config/parameterConfigSaveApi.py
+++ apps/AllSystemData/DasSystem/das_api/param_config/parameterConfigSaveApi.py
@@ -8,16 +8,15 @@ from apps.AllSystemData.DasSystem.das_api.dasSystem_interface_url import DasApiU
 from apps.Common_Config.interface_common_info import Common_TokenHeader
 from apps.AllSystemData.DasSystem.das_api.dasSystem_interface_param import DasApiInputParam
 from apps.get_page_content_by_requests import get_page_content_by_requests
-from logger import MyLog
+from flask import current_app as app
 
-# 实例化日志类
-logger = MyLog("ParameterConfigApi").getlog() # 初始化
+
 # 参数配置页面接口服务
 class ParameterConfigApi():
     def paramConfigFunction(self,paramStr):
-        logger.info("paramConfigFunction ---->start!")
+        app.logger.info("paramConfigFunction ---->start!")
         if paramStr == "":
-            logger.error("paramConfigFunction --> request parameters is wrong!")
+            app.logger.error("paramConfigFunction --> request parameters is wrong!")
             return "请求参数为空"
 
         # 接口地址
@@ -36,9 +35,9 @@ class ParameterConfigApi():
         self.header = header
         resp = get_page_content_by_requests(self.url, self.header, self.formData)
         if resp.status_code == 200:
-            logger.info("paramConfigFunction ---->end!")
+            app.logger.info("paramConfigFunction ---->end!")
             return "取消开发备注---保存接口响应成功"
         else:
-            logger.error("paramConfigFunction -->response Data is wrong!")
+            app.logger.error("paramConfigFunction -->response Data is wrong!")
             return "接口响应失败,失败原因:{0},接口地址:{1},请求参数:{2}".format(resp.json()["errorMsg"],url,reqParam)
 
