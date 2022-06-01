efa9a2e2b432b212afc3e33ab70c3f583d532341
quanliu <13923832556@163.com>
2022-06-01 18:37:26 +0800

    update

--- logger.py
+++ logger.py
@@ -9,62 +9,62 @@ import logging.handlers
 import os,multiprocessing
 from logging.handlers import TimedRotatingFileHandler
 
-
-class SafeLog(TimedRotatingFileHandler):
-    """
-    因为TimedRotatingFileHandler在多进程访问log文件时，切分log日志会报错文件被占用，所以修复这个问题
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(SafeLog, self).__init__(*args, **kwargs)
-        self.suffix_time = ""
-        self.origin_basename = self.baseFilename
-
-    def shouldRollover(self, record):
-        time_tuple = time.localtime()
-        if self.suffix_time != time.strftime(self.suffix, time_tuple) or not os.path.exists(
-                self.origin_basename + '.' + self.suffix_time):
-            return 1
-        else:
-            return 0
-
-    def doRollover(self):
-        if self.stream:
-            self.stream.close()
-            self.stream = None
-
-        current_time_tuple = time.localtime()
-        self.suffix_time = time.strftime(self.suffix, current_time_tuple)
-        self.baseFilename = self.origin_basename + '.' + self.suffix_time
-
-        self.mode = 'a'
-
-        with multiprocessing.Lock():
-            if self.backupCount > 0:
-                for s in self.getFilesToDelete():
-                    os.remove(s)
-
-        if not self.delay:
-            self.stream = self._open()
-
-    def getFilesToDelete(self):
-        # 将源代码的 self.baseFilename 改为 self.origin_basename
-        dir_name, base_name = os.path.split(self.origin_basename)
-        file_names = os.listdir(dir_name)
-        result = []
-        prefix = base_name + "."
-        p_len = len(prefix)
-        for fileName in file_names:
-            if fileName[:p_len] == prefix:
-                suffix = fileName[p_len:]
-                if self.extMatch.match(suffix):
-                    result.append(os.path.join(dir_name, fileName))
-        if len(result) < self.backupCount:
-            result = []
-        else:
-            result.sort()
-            result = result[:len(result) - self.backupCount]
-        return result
+#
+# class SafeLog(TimedRotatingFileHandler):
+#     """
+#     因为TimedRotatingFileHandler在多进程访问log文件时，切分log日志会报错文件被占用，所以修复这个问题
+#     """
+#
+#     def __init__(self, *args, **kwargs):
+#         super(SafeLog, self).__init__(*args, **kwargs)
+#         self.suffix_time = ""
+#         self.origin_basename = self.baseFilename
+#
+#     def shouldRollover(self, record):
+#         time_tuple = time.localtime()
+#         if self.suffix_time != time.strftime(self.suffix, time_tuple) or not os.path.exists(
+#                 self.origin_basename + '.' + self.suffix_time):
+#             return 1
+#         else:
+#             return 0
+#
+#     def doRollover(self):
+#         if self.stream:
+#             self.stream.close()
+#             self.stream = None
+#
+#         current_time_tuple = time.localtime()
+#         self.suffix_time = time.strftime(self.suffix, current_time_tuple)
+#         self.baseFilename = self.origin_basename + '.' + self.suffix_time
+#
+#         self.mode = 'a'
+#
+#         with multiprocessing.Lock():
+#             if self.backupCount > 0:
+#                 for s in self.getFilesToDelete():
+#                     os.remove(s)
+#
+#         if not self.delay:
+#             self.stream = self._open()
+#
+#     def getFilesToDelete(self):
+#         # 将源代码的 self.baseFilename 改为 self.origin_basename
+#         dir_name, base_name = os.path.split(self.origin_basename)
+#         file_names = os.listdir(dir_name)
+#         result = []
+#         prefix = base_name + "."
+#         p_len = len(prefix)
+#         for fileName in file_names:
+#             if fileName[:p_len] == prefix:
+#                 suffix = fileName[p_len:]
+#                 if self.extMatch.match(suffix):
+#                     result.append(os.path.join(dir_name, fileName))
+#         if len(result) < self.backupCount:
+#             result = []
+#         else:
+#             result.sort()
+#             result = result[:len(result) - self.backupCount]
+#         return result
 
 
 
@@ -86,7 +86,7 @@ def setup_log(config):
     #       12点，就会创建新的日志。
     # interval：是指等待多少个单位 when 的时间后，Logger会自动重建文件。
     # backupCount：是保留日志个数。默认的0是不会自动删除掉日志。
-    file_log_handler = SafeLog(filename=log_name, interval=1, backupCount=10, when="D",encoding='UTF-8')
+    file_log_handler = logging.handlers.TimedRotatingFileHandler(filename=log_name, interval=1, backupCount=10, when="D",encoding='UTF-8')
     # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
     formatter = logging.Formatter('[%(asctime)s] %(filename)s->%(funcName)s line:%(lineno)d [%(levelname)s]%(message)s')
     # 为刚创建的日志记录器设置日志记录格式
