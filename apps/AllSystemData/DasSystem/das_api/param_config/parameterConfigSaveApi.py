5df0ff05135e0df3cf2c52de26bcbb868f80e41e
quanliu <13923832556@163.com>
2022-06-06 14:20:49 +0800

    更新接口

--- apps/AllSystemData/DasSystem/das_api/param_config/parameterConfigSaveApi.py
+++ apps/AllSystemData/DasSystem/das_api/param_config/parameterConfigSaveApi.py
@@ -5,39 +5,24 @@
 @Desc:数据分析-参数配置页面接口服务
 '''
 from apps.AllSystemData.DasSystem.das_api.dasSystem_interface_url import DasApiUrl
-from apps.Common_Config.interface_common_info import Common_TokenHeader
 from apps.AllSystemData.DasSystem.das_api.dasSystem_interface_param import DasApiInputParam
-from apps.get_page_content_by_requests import get_page_content_by_requests
+from apps.Common_Config.operate_api_data import api_assemble_new
 from flask import current_app as app
 
 
 # 参数配置页面接口服务
-class ParameterConfigApi():
-    def paramConfigFunction(self,paramStr):
-        app.logger.info("paramConfigFunction ---->start!")
-        if paramStr == "":
-            app.logger.error("paramConfigFunction --> request parameters is wrong!")
-            return "请求参数为空"
+@api_assemble_new()
+def paramConfigFunction(paramStr):
+    app.logger.info("paramConfigFunction ---->start!")
+    if paramStr == "":
+        app.logger.error("paramConfigFunction --> request parameters is wrong!")
+        return "请求参数为空"
 
-        # 接口地址
-        url = DasApiUrl.paramConfigSave_url
-        # 拼接接口请求入参
-        reqSelect = DasApiInputParam.paramConfig_select
-        reqSelectStr = reqSelect.replace("{notesInfoList}", paramStr)  # 替换参数
-        reqParam = DasApiInputParam.paramConfig_param
-        reqParam["args"] = reqSelectStr
-
-        # 接口请求头
-        header = Common_TokenHeader().token_header("new","181324")
-        # 组装接口所需要的参数
-        self.url = url
-        self.formData = reqParam
-        self.header = header
-        resp = get_page_content_by_requests(self.url, self.header, self.formData)
-        if resp.status_code == 200:
-            app.logger.info("paramConfigFunction ---->end!")
-            return "取消开发备注---保存接口响应成功"
-        else:
-            app.logger.error("paramConfigFunction -->response Data is wrong!")
-            return "接口响应失败,失败原因:{0},接口地址:{1},请求参数:{2}".format(resp.json()["errorMsg"],url,reqParam)
-
+    # 接口地址
+    url = DasApiUrl.paramConfigSave_url
+    # 拼接接口请求入参
+    reqSelect = DasApiInputParam.paramConfig_select
+    reqSelectStr = reqSelect.replace("{notesInfoList}", paramStr)  # 替换参数
+    reqParam = DasApiInputParam.paramConfig_param
+    reqParam["args"] = reqSelectStr
+    return url,reqParam
--- apps/AllSystemData/DasSystem/das_api/param_config/parameterConfigSelectApi.py
+++ apps/AllSystemData/DasSystem/das_api/param_config/parameterConfigSelectApi.py
@@ -5,31 +5,16 @@
 @Desc:参数配置-取消开发备注查询接口服务
 '''
 from apps.AllSystemData.DasSystem.das_api.dasSystem_interface_url import DasApiUrl
-from apps.Common_Config.interface_common_info import Common_TokenHeader
 from apps.AllSystemData.DasSystem.das_api.dasSystem_interface_param import DasApiInputParam
-from apps.get_page_content_by_requests import get_page_content_by_requests
+from apps.Common_Config.operate_api_data import api_assemble_new
 from flask import current_app as app
 
 
-
-class ParameterConfigQueryApi():
-    def paramConfigQuery(self):
-        app.logger.info("paramConfigQuery ---->start!")
-        # 接口地址
-        url = DasApiUrl.paramConfigSelect_url
-        # 接口请求参数
-        formData = DasApiInputParam.paramConfigQuery
-        # 接口请求头
-        header = Common_TokenHeader().token_header("new","181324")
-        self.url = url
-        self.formData = formData
-        self.header = header
-        # 响应值
-        respResult = get_page_content_by_requests(self.url,self.header,self.formData)
-        responseData = respResult.json()["result"]["cancelDevNotesInfoList"]
-        if respResult.status_code == 200:
-            app.logger.info("paramConfigQuery ---->end!")
-            return "接口响应成功,接口返回值:{0}".format(responseData)
-        else:
-            app.logger.error("paramConfigQuery -->response Data is wrong!")
-            return "接口响应失败,失败原因:{0},接口地址:{1},请求参数:{2}".format(respResult.json()["errorMsg"], url, formData)
+@api_assemble_new()
+def paramConfigQuery():
+    app.logger.info("paramConfigQuery ---->start!")
+    # 接口地址
+    url = DasApiUrl.paramConfigSelect_url
+    # 接口请求参数
+    formData = DasApiInputParam.paramConfigQuery
+    return url,formData
