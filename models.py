7fdd3a95a2e032836115387cff2853d6937385a2
quanliu <13923832556@163.com>
2022-06-06 17:29:11 +0800

    update

--- models.py
+++ models.py
@@ -5,15 +5,16 @@
 @Desc:数据库建表模型类（必须要被引入）
 '''
 from dbExat import db
+import datetime
 
 
 class InterfaceResultModel(db.Model):
     __bind_key__ = "test_db" # 绑定数据库
     __tablename__ = "interface_result_info" # 接口自动化结果表
-    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
     modelName = db.Column(db.String(20),comment="模块名")
     systemName = db.Column(db.String(80), unique=True,comment="系统名称")
     testCaseNum = db.Column(db.Integer,comment="总的用例数")
     successCaseNum = db.Column(db.Integer,comment="执行成功数")
     failCaseNum = db.Column(db.Integer,comment="失败数")
     reportUrl = db.Column(db.String(500),comment="报告地址")
+    updateTime = db.Column(db.DateTime,default=datetime.datetime.now(),onupdate=datetime.datetime.now())
