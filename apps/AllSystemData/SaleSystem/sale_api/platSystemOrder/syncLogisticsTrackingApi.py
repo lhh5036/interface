69f6e12cd14296c02175f6e9c4a5c537192c0c24
quanliu <13923832556@163.com>
2022-06-07 15:44:24 +0800

    update

--- apps/AllSystemData/SaleSystem/sale_api/platSystemOrder/syncLogisticsTrackingApi.py
+++ apps/AllSystemData/SaleSystem/sale_api/platSystemOrder/syncLogisticsTrackingApi.py
@@ -1,5 +1,5 @@
 '''
-@File: syncLogisticsTracking.py
+@File: syncLogisticsTrackingApi.py
 @time:2022/6/7
 @Author:quanliu 181324
 @Desc:同步追踪号
@@ -10,14 +10,14 @@ from apps.AllSystemData.SaleSystem.sale_api.saleSystem_interface_param import Sa
 from apps.AllSystemData.SaleSystem.sale_api.saleSystem_interface_url import SaleApiUrl
 from apps.Common_Config.operate_api_data import api_assemble_new
 import json
-# from flask import current_app as app
+from flask import current_app as app
 
 # 系统订单查询接口
 @api_assemble_new(login_method="old")
 def syncLogisticsTrackingFunc(platform,paramList):
-    # app.logger.info("systemOrderQueryFun  ----->start!")
+    app.logger.info("systemOrderQueryFun  ----->start!")
     if platform == "" or len(paramList) <= 0:
-        # app.logger.error("systemOrderQueryFun ------>request parameter is wrong!")
+        app.logger.error("systemOrderQueryFun ------>request parameter is wrong!")
         return "请求参数为空"
 
     # 获取url
@@ -28,6 +28,3 @@ def syncLogisticsTrackingFunc(platform,paramList):
     param02 = paramList
     param01["args"] = json.dumps(param02)
     return url,param01
-
-if __name__ == '__main__':
-    print(syncLogisticsTrackingFunc("SMT",["YSTN2206071586266573","YSTN2206071586266537"]))
