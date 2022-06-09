29f8d7f8afb5c68289ec3d9526e03d05b2d50530
quanliu <13923832556@163.com>
2022-06-09 10:23:29 +0800

    update

---
+++ apps/AllSystemData/WmsSystem/wms_api/wmsSystem_interface_param.py
@@ -0,0 +1,9 @@
+'''
+@File: wmsSystem_interface_param.py
+@time:2022/6/9
+@Author:quanliu 181324
+@Desc:仓库系统接口参数
+'''
+
+
+class WmsApiInputParam:
---
+++ apps/AllSystemData/WmsSystem/wms_api/wmsSystem_interface_url.py
@@ -0,0 +1,10 @@
+'''
+@File: wmsSystem_interface_url.py
+@time:2022/6/9
+@Author:quanliu 181324
+@Desc:仓库系统接口地址
+'''
+
+
+class WmsApiUrl:
+    
