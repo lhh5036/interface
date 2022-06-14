9de95c4929e4420eebac465e2782e8d3a9189c87
quanliu <13923832556@163.com>
2022-06-14 18:33:12 +0800

    update

--- models.py
+++ models.py
@@ -20,3 +20,20 @@ class InterfaceResultModel(db.Model):
     failCaseNum = db.Column(db.Integer,comment="失败数")
     reportUrl = db.Column(db.String(500),comment="报告地址")
     updateTime = db.Column(db.DateTime,default=datetime.datetime.now(),onupdate=datetime.datetime.now())
+
+
+# 一对一
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(32), unique=True)
+#     userdata = db.relationship('UserData', uselist=False, back_populates='user')
+#
+# class UserData(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(200))
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     user = db.relationship('User', back_populates=('userdata'))
+#
+# 1.上述代码中的relationship，是关联属性的意思，是SQLAlchemy提供给开发者快速引用外键模型的一个对象属性，本身并不存在于MySQL中；
+# 2.relationship的参数backref表示反向引用，通过外键模型查询主模型数据时的关联属性，通俗的讲就是在查DeviceDetail数据时，可以通过backref引用到Devices。
+# 3.useList表示关联模型是否为List，如果为False，则不使用列表，而使用标量值。一对一关系中，需要设置relationship中的uselist=Flase
