2309ce05f96ce99d0050f7cb5caffb2b3e950ee9
quanliu <13923832556@163.com>
2022-06-14 18:41:51 +0800

    update

--- apps/AllSystemData/updateInterResult.py
+++ apps/AllSystemData/updateInterResult.py
@@ -3,8 +3,16 @@
 @time:2022/6/6
 @Author:quanliu 181324
 @Desc:默认filter是且的关系，如果使用or需要导入from sqlalchemy import or_；用法：filter(or_(User.name=="lisi",User.password==123))
-排序：User.query.order_by(User.id.asc()).all()
+排序：User.query.order_by(User.id.asc()).all()   User.query.order_by(User.id.desc()).all()
 分组:db.session.query(User.role_id,func.count(User.role_id)).group_by(User.role_id).all() ---导入：from sqlalchemy import func
+
+查询第一行:
+User.query.filter(User.name=="lili").first()
+User.query.get(2) -->精确获取第二行的数据
+模糊查询：
+from sqlalchemy import or_
+objs = User.query.filter(User.name.like('%{keyword}%'.format(keyword=keyword))).all()-->区分大小写
+objs = User.query.filter(User.name.ilike('%{keyword}%'.format(keyword=keyword))).all() -->不区分大小写
 '''
 from models import InterfaceResultModel # 引入模板类
 from dbExat import db
