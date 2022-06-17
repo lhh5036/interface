c5bf9d7598cb50a3797f683f5d698062fff91dfc
quanliu <13923832556@163.com>
2022-06-17 11:16:05 +0800

    update

--- apps/utils/assert_new_utils.py
+++ apps/utils/assert_new_utils.py
@@ -9,54 +9,54 @@ from deepdiff import DeepDiff
 
 def new_assert_utils(func): # 接口返回的数据和期望的数据 [状态码,响应结果数据]
     def wragger(*args, **kwargs):
-        checkResult = True # 默认校验通过
         resp_result,expResp_result = func(*args, **kwargs) # 获取结果
         if len(resp_result) < 2:
-            logging.error("需要断言的数据不全，请检查!")
-            checkResult = False
-            return checkResult
+            print("需要断言的数据不全，请检查!")
+            return False
         if len(expResp_result) < 2:
-            logging.error("期望的数据不全，请检查!")
-            checkResult = False
-            return checkResult
+            print("期望的数据不全，请检查!")
+            return False
 
         resp_statusCode = resp_result[0] # 接口返回响应状态码
         resp_json = resp_result[1] # 接口返回响应结果
         exp_resp_statusCode = expResp_result[0] # 接口期望状态码
         exp_resp_json = expResp_result[1] # 接口期望返回的结果
-        comp_code = assert_statusCode(resp_statusCode,exp_resp_statusCode)
-        if checkResult != comp_code: # 如果校验状态码的结果与期望结果不一致
-            checkResult = False
-            return checkResult
+        comp_code_msg = assert_statusCode(resp_statusCode,exp_resp_statusCode) # 获取响应状态码的校验信息
+        if comp_code_msg != "": # 如果校验状态码的结果与期望结果不一致
+            print(comp_code_msg)
+            return False
         else:
-            comp_json = assert_respJson(resp_json, exp_resp_json)
-            if checkResult == comp_json:
-                return checkResult
+            comp_json_msg = assert_respJson(resp_json, exp_resp_json) # 获取响应结果中某些值得校验信息
+            if comp_json_msg != "":
+                print("响应结果中的参数值与期望的结果不一致,对比结果:{0}".format(comp_json_msg))
+                return False
             else:
-                checkResult = False
-                return checkResult
+                print("响应结果中的参数值与期望结果一致")
+                return True
     return wragger
 
 # 断言响应状态码
 def assert_statusCode(resp_statusCode, exp_resp_statusCode):
+    msg = ""
     if resp_statusCode != exp_resp_statusCode:
-        logging.error("接口状态码与期望状态码不一致!接口状态码为:{0}".format(resp_statusCode))
-        return False
-    else:
-        logging.info("接口状态码与期望状态码一致")
-        return True
+        msg = "接口状态码与期望状态码不一致!接口状态码为:{0},期望状态码:{1}".format(resp_statusCode,exp_resp_statusCode)
+        return msg
+    logging.info("接口状态码与期望状态码一致")
+    return msg
 
 # 断言响应结果，默认都会按照JSON格式来校验
 def assert_respJson(resp_json, exp_resp_json):
+    msg = ""
     if resp_json == {} or exp_resp_json == {}:
-        return "需要校验的数据或者期望数据结果为空，请检查!"
+        msg = "需要校验的数据或者期望数据结果为空，请检查!"
+        return msg
     ddiff = DeepDiff(resp_json,exp_resp_json, ignore_string_case=True, ignore_order=True, view="tree")
     if ddiff == {}:
         logging.info("接口响应结果与期望一致")
-        return True
+        return msg
     else:
-        logging.error("接口响应结果与期望结果不一致,请检查:{0}".format(ddiff))
-        return False
+        msg = "接口响应结果与期望结果不一致,请检查:{0}".format(ddiff)
+        return msg
 
 # 传入要校验的值，接口响应结果，期望值(SQL查询结果)
 def concat_assert_data(func):
@@ -102,9 +102,9 @@ def concat_assert_data(func):
 def test():
     t1 = ["a","b"]
     t2 = [200,{"a":"1","b":2,"c":3}]
-    t3 = [500,((1,2),)]
+    t3 = [200,((1,2),)]
 
     return t1,t2,t3
 
 if __name__ == '__main__':
-    print(test())
+    test()
