766ba16aa15f6f68065362cf55d113aea8f386b7
quanliu <13923832556@163.com>
2022-06-17 10:07:25 +0800

    updaet

--- apps/utils/assert_new_utils.py
+++ apps/utils/assert_new_utils.py
@@ -42,10 +42,60 @@ def assert_statusCode(resp_statusCode,exp_resp_statusCode):
 def assert_respJson(resp_json,exp_resp_json):
     if resp_json == {} or exp_resp_json == {}:
         return "需要校验的数据或者期望数据结果为空，请检查!"
-    ddiff = DeepDiff(resp_json,exp_resp_json,ignore_string_case=True,ignore_order=True )
+    ddiff = DeepDiff(resp_json,exp_resp_json,ignore_string_case=True,ignore_order=True,view="tree")
     if ddiff == {}:
         logging.info("接口响应结果与期望一致")
         return True
     else:
         logging.error("接口响应结果与期望结果不一致,请检查:{0}".format(ddiff))
         return False
+
+# 传入要校验的值，接口响应结果，期望值(SQL查询结果)
+def concat_assert_data(func):
+    def wragger(*args):
+        keysList, resp_json, exp_select_result = func(*args) # resp_json--》接口响应状态码和响应结果；exp_select_result-->期望状态码和SQL查询结果
+        check_json_list = [] # 需要校验的值
+        expact_json_list = [] # 期望的结果
+        if len(keysList) <= 0 or len(resp_json) < 2 or len(exp_select_result) < 2:
+            logging.error("没有需要校验的key")
+            return check_json_list,expact_json_list
+        check_code = resp_json[0] # 响应状态码
+        check_json = resp_json[1] # 响应结果
+        result_check_json = {}
+        for key in keysList:
+            value_list = []
+            value_list.append(check_json[key])
+            result_check_json[key] = value_list  # 将需要校验的key单独存，且value为List
+        # 拼接需要获取需要校验的数据
+        check_json_list.append(check_code)
+        check_json_list.append(result_check_json)
+
+        # 处理期望值
+        expact_code = exp_select_result[0] # 期望状态码
+        expact_json = exp_select_result[1] # 期望SQL查询结果
+        if len(expact_json) <= 0:
+            logging.error("期望结果为空")
+            return check_json_list,expact_json_list
+        m = 0
+        expact_json_dict = {}
+        for k in keysList:
+            expact_json_dict[k] = []
+            for i in range(len(expact_json)):
+                expact_json_dict[k].append(expact_json[i][m])
+            m += 1
+        # 拼接期望数据
+        expact_json_list.append(expact_code)
+        expact_json_list.append(expact_json_dict)
+        return check_json_list,expact_json_list
+    return wragger
+
+@concat_assert_data
+def test():
+    t1 = ["a","b"]
+    t2 = [200,{"a":"1","b":2,"c":3}]
+    t3 = [200,((1,2),(4,5),(6,7))]
+
+    return t1,t2,t3
+
+if __name__ == '__main__':
+    print(test())
