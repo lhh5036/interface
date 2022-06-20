56f5d04fdad03f934a9b4c953d7da469981d6f75
quanliu <13923832556@163.com>
2022-06-20 10:10:06 +0800

    更新接口拼接

--- apps/AllSystemData/PmsSystem/pms_api/menuManager/purchaseSuggestionApi.py
+++ apps/AllSystemData/PmsSystem/pms_api/menuManager/purchaseSuggestionApi.py
@@ -4,35 +4,26 @@
 @Author: quanliu 181324
 @Desc: 需求管理-采购建议接口
 '''
-from copy import deepcopy
 from apps.AllSystemData.PmsSystem.pms_api.pmsSystem_interface_param import PmsApiInputParam
 from apps.AllSystemData.PmsSystem.pms_api.pmsSystem_interface_url import PmsApiUrl
-from apps.Common_Config.operate_api_data import api_assemble_new
-import json
-from apps.Common_Config.parseRequestDatas import parseRequestDatas
-# from flask import current_app as app
+from apps.Common_Config.operate_api_data import api_assemble_new, Splicing_Params
+from flask import current_app as app
 
 # 系统订单查询接口
 @api_assemble_new()
 def purchaseSuggestionApi(paramMap=None):
-    # app.logger.info("purchaseSuggestionApi  ----->start!")
+    app.logger.info("purchaseSuggestionApi  ----->start!")
     url = PmsApiUrl.purchaseSuggestion_url
-    param03 = deepcopy(PmsApiInputParam.purchaseSuggestion_param03)
-    keyList = []
-    if paramMap != None:
-        for key in paramMap.keys():
-            keyList.append(key)
-        for i in range(len(keyList)):
-            value = parseRequestDatas(keyList[i], paramMap)
-            param03[keyList[i]] = value
-
     param01 = PmsApiInputParam.purchaseSuggestion_param01
-    param02 = PmsApiInputParam().purchaseSuggestion_param02
-
-    param02["search"] = param03
-    param01["args"] = json.dumps(param02)
-    return url,param01
+    param02 = PmsApiInputParam.purchaseSuggestion_param02
+    param03 = PmsApiInputParam.purchaseSuggestion_param03
+    paramList = []
+    paramList.append(param01)
+    paramList.append(param02)
+    paramList.append(param03)
+    requestJson = Splicing_Params(paramList,paramMap).splicing_params() # 拼接接口参数的方法
+    return url,requestJson
 
 if __name__ == '__main__':
-    print(purchaseSuggestionApi())
-
+    print(purchaseSuggestionApi({"warehouseAttr":"美景仓"}))
+    print(purchaseSuggestionApi({"articleNumber":"5AC304821-A03"}))
