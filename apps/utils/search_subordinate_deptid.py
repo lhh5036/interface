2a97efbbeabb27a0f2d554274411c92b83f69184
majiaqin <majiaqin@techb.top>
2022-06-27 17:37:13 +0800

    增加递归查询组织架构id的全部上级id

--- apps/utils/search_subordinate_deptid.py
+++ apps/utils/search_subordinate_deptid.py
@@ -4,7 +4,7 @@
 @File: search_subordinate_deptid
 @time:2022/6/23
 @Author:majiaqin 170479
-@Desc:递归查询组织架构id的全部下级id
+@Desc:递归查询组织架构id
 '''
 import pprint
 from apps.AllSystemData.UsermgtSystem.usermgt_common_setting import Usermgt_Common_Setting
@@ -21,6 +21,15 @@ class Search_Subordinate_Deptid():
 WHERE parent_id = '{0}';".format(self.deptId)
         return sql
 
+    @convert_list
+    @get_data(Usermgt_Common_Setting.usermgt_mysql)
+    def get_superdeptId(self, deptId):
+        self.deptId = deptId
+        sql = "SELECT parent_id FROM org_struct \
+WHERE id = '{0}';".format(self.deptId)
+        return sql
+
+    '''递归查询组织架构id的全部下级id'''
     def search_subordinate_deptid(self):
         for deptId in self.deptid_list:
             deptIdList = self.get_deptId(deptId)
@@ -29,7 +38,22 @@ WHERE parent_id = '{0}';".format(self.deptId)
                 self.get_deptId(deptIdList)
             else:
                 continue
-        return list(set(self.deptid_list))
+        dept_list = []
+        for i in self.deptid_list:
+            if i not in dept_list:
+                dept_list.append(i)
+        return dept_list
+
+    '''递归查询组织架构id的全部上级id'''
+    def search_subordinate_superdeptid(self):
+        for deptId in self.deptid_list:
+            deptIdList = self.get_superdeptId(deptId)
+            if len(deptIdList) > 0:
+                self.deptid_list += deptIdList
+                self.get_superdeptId(deptIdList)
+            else:
+                continue
+        return self.deptid_list
 
 
 if __name__ == '__main__':
