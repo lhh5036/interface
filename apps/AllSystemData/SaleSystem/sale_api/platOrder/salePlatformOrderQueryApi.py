77878808615c922b40a89492a13b1b96fc249fdc
苹果 <972302199@qq.com>
2022-07-07 17:17:28 +0800

    SMT平台订单-查询“等待您发货”状态订单Api

--- apps/AllSystemData/SaleSystem/sale_api/platOrder/salePlatformOrderQueryApi.py
+++ apps/AllSystemData/SaleSystem/sale_api/platOrder/salePlatformOrderQueryApi.py
@@ -8,26 +8,28 @@
 @Desc: 销售系统-平台订单查询接口
 '''
 import json
+import copy
 from apps.AllSystemData.SaleSystem.sale_api.saleSystem_interface_param import SaleApiInputParam
 from apps.AllSystemData.SaleSystem.sale_api.saleSystem_interface_url import SaleApiUrl
 from apps.Common_Config.operate_api_data import api_assemble_new, Common_TokenHeader
 from loggerUtils import MyLog
 
 logger = MyLog("salePlatformOrderApi").getlog()
-@api_assemble_new(api_header=Common_TokenHeader().token_header('new', '10338'))
-def salePlatformOrderApi(orderStatus):
+@api_assemble_new(api_header=Common_TokenHeader().token_header('new', '031'))
+def salePlatformOrderApi(field,orderStatus):
     logger.info("salePlatformOrderApi ---->start!")
     url = SaleApiUrl.aliexpressOrderQuery_url
     param01 = SaleApiInputParam.smtQueryStatus_param01
     param02 = SaleApiInputParam.smtQueryStatus_param02
-    param03 = SaleApiInputParam.smtQueryStatus_param03
-    param03["orderStatus"] = orderStatus
+    param03 = copy.deepcopy(SaleApiInputParam.smtQueryStatus_param03) # 深复制：该变量及其内所有可变对象重新分配地址。修改任意值，原变量都不会发生改变
+    # param03 = SaleApiInputParam.smtQueryStatus_param03  # 不会初始化param03对象
+    param03[field] = orderStatus
     param02["search"] = param03
     param01["args"] = json.dumps(param02)
-    reqParam = param01
-    return url, reqParam
+    return url, param01
 
 if __name__ == '__main__':
-    print(salePlatformOrderApi("WAIT_SELLER_SEND_GOODS"))
+    print(salePlatformOrderApi("accountNumber", "szhesitong1902@163.com"))
+    print(salePlatformOrderApi("orderStatus","WAIT_SELLER_SEND_GOODS"))
+    print(salePlatformOrderApi("orderId", "8151578618327041"))
     # print(salePlatformOrderApi("WAIT_SELLER_SEND_GOODS"))
-    # print(salePlatformOrderApi({"accountNumber":"szhesitong1902@163.com"}))
--- apps/AllSystemData/SaleSystem/test/case/test_salePlatformOrderQueryApi.py
+++ apps/AllSystemData/SaleSystem/test/case/test_salePlatformOrderQueryApi.py
@@ -16,21 +16,56 @@ from apps.AllSystemData.SaleSystem.sale_api.platOrder.salePlatformOrderQueryApi 
 @ddt.ddt
 class Test_salePlatformOrderQueryApi(unittest.TestCase):
     def testCase01(self):
+        # api返回json结果提取
+        api_retest = salePlatformOrderApi("accountNumber", "13009005919@163.com")  # 查询店铺账号——13009005919@163.com SMT平台订单
+        api_reCode = api_retest[0]
+        api_reJsonValue = api_retest[1]["total"]
+
+        # 预期达到的结果
+        expected_resultsCode = 200
+        sql = "select count(id) from aliexpress_order where account_number = '13009005919@163.com';"
+        expected_resultsValue = Mysql_handleOperator(Sale_Common_Setting.sale_mysql).data_sql("select", sql)
+
+        # 将预期结果和api结果返回——方便进行下一步比较
+        api_test = [api_reCode, api_reJsonValue]  # 接口返回的code码以及‘total’字段的值
+        expected_test = [expected_resultsCode, expected_resultsValue]  # 预期返回200以及数据库查询到的总计条数
+        print("\napi返回结果：", api_test, "\n预期结果:", expected_test)
+        return api_test, expected_test
+
+    def testCase02(self):
+        # api返回json结果提取
+        api_retest = salePlatformOrderApi("orderStatus","WAIT_SELLER_SEND_GOODS") # 查询订单状态——等待您发货 SMT平台订单
+        api_reCode = api_retest[0]
+        api_reJsonValue = api_retest[1]["total"]
+
         # 预期达到的结果
         expected_resultsCode = 200
         sql = "select count(id) from aliexpress_order where order_status = 'WAIT_SELLER_SEND_GOODS';"
         expected_resultsValue = Mysql_handleOperator(Sale_Common_Setting.sale_mysql).data_sql("select", sql)
-
-        # api返回json结果提取
-        api_retest = salePlatformOrderApi("WAIT_SELLER_SEND_GOODS")
-        api_reCode = api_retest[0]
-        api_reJsonValue = api_retest[1]["total"]
 
         # 将预期结果和api结果返回——方便进行下一步比较
         api_test = [api_reCode,api_reJsonValue] # 接口返回的code码以及‘total’字段的值
         expected_test = [expected_resultsCode,expected_resultsValue] # 预期返回200以及数据库查询到的总计条数
         print("\napi返回结果：",api_test,"\n预期结果:",expected_test)
         return api_test,expected_test
+
+    def testCase03(self):
+        # api返回json结果提取
+        api_retest = salePlatformOrderApi("orderId", "5030256190081355")  # 查询平台单号——5030256190081355 SMT平台订单
+        api_reCode = api_retest[0]
+        api_reJsonValue = api_retest[1]["total"]
+
+        # 预期达到的结果
+        expected_resultsCode = 200
+        sql = "select count(id) from aliexpress_order where order_id = '5030256190081355';"
+        expected_resultsValue = Mysql_handleOperator(Sale_Common_Setting.sale_mysql).data_sql("select", sql)
+
+        # 将预期结果和api结果返回——方便进行下一步比较
+        api_test = [api_reCode, api_reJsonValue]  # 接口返回的code码以及‘total’字段的值
+        expected_test = [expected_resultsCode, expected_resultsValue]  # 预期返回200以及数据库查询到的总计条数
+        print("\napi返回结果：", api_test, "\n预期结果:", expected_test)
+        return api_test, expected_test
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
