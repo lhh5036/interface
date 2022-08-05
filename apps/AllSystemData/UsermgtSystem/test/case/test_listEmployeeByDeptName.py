abe0d267d72add3c1ee767376f3e7f0dea4b19ba
majiaqin <majiaqin@techb.top>
2022-08-05 15:38:16 +0800

    增加hr数据库配置

---
+++ apps/AllSystemData/UsermgtSystem/test/case/test_listEmployeeByDeptName.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: test_listEmployeeByDeptName
+@time:2022/8/5
+@Author:majiaqin 170479
+@Desc:
+'''
---
+++ apps/AllSystemData/UsermgtSystem/usermgt_api/usermgt_api_ware/listEmployeeByDeptName.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: listEmployeeByDeptName
+@time:2022/8/5
+@Author:majiaqin 170479
+@Desc:
+'''
